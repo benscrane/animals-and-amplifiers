8526b166a6b907a82ab38330bb31306b0bd5f062
Ben Crane <bencrane5.12@gmail.com>
2019-09-25 14:32:14 -0400

    Add about link

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     title: 'Built by Ben',
-    description: 'A portfolio site for Ben Crane'
+    description: 'A portfolio site for Ben Crane',
+    themeConfig: {
+        nav: [
+            { text: 'About', link: '/about/index.html'}
+        ]
+    }
 }
